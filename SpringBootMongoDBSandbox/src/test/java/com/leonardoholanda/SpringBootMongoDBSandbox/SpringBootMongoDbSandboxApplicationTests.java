42aa4d5f00dbd0184354dd2c4e7f550f6fca683e
Leonardo Holanda e Silva <44146880+NachoNardo@users.noreply.github.com>
2023-06-08 20:42:22 -0300

    First commit

---
+++ SpringBootMongoDBSandbox/src/test/java/com/leonardoholanda/SpringBootMongoDBSandbox/SpringBootMongoDbSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardoholanda.SpringBootMongoDBSandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongoDbSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
